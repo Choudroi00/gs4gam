e754e4b5fd8bd3921a0259933a3adb3201e818c0
mounirzx <102691929+mounirzx@users.noreply.github.com>
2022-06-21 14:40:04 +0100

    Delete api.php

--- api.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-$data = json_decode(file_get_contents("php://input"),true);
-?>
