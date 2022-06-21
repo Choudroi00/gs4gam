1d89e27b1d00460774d58da1c66409cafbd3c23b
mounirzx <102691929+mounirzx@users.noreply.github.com>
2022-06-21 14:39:26 +0100

    Add files via upload

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$data = json_decode(file_get_contents("php://input"),true);
+?>
