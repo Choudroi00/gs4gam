33e0e55dfd6e2f99017ae4590193b590b17f3386
Choudroi00 <54903169+Choudroi00@users.noreply.github.com>
2022-07-12 14:27:02 -0400

    Update auth.php

--- AuthSystem/auth.php
+++ AuthSystem/auth.php
@@ -1,25 +1,24 @@
 <?php
-include ('./db.php');
+require('./db.php');
 
 
 $letters = 'abcdefghijklmnopqrstuvwxyz';
-$capLetters = strtoupper($letters) ;
+$capLetters = strtoupper($letters);
 $num = '123456789_';
-$dic = str_split( $letters.$capLetters.$num );
+$dic = str_split($letters . $capLetters . $num);
 
-print_r(json_encode(handelData(array($_POST['auth'],$_POST['username'],$_POST['password'],$_POST['email'],$_POST['phone']),$mainDB,$dic)));
+print_r(json_encode(handelData(array($_POST['auth'], $_POST['username'], $_POST['password'], $_POST['email'], $_POST['phone']), $conn, $dic)));
 
-
-function handelData($data,$db,$dic)
+function handelData($data, $db, $dic)
 {
     switch ($data[0]) {
         case 'login':
-            return login(array($data[1],md5($data[2]),''),$db,$dic);
+            return login(array($data[1], md5($data[2]), ''), $db, $dic);
             break;
         case 'register':
-            return register(array($data[1],md5($data[2]),$data[3],$data[4]),$db,$dic);
+            return register(array($data[1], md5($data[2]), $data[3], $data[4]), $db, $dic);
             break;
-        
+
         default:
             # code...
             break;
@@ -27,18 +26,19 @@ function handelData($data,$db,$dic)
 }
 
 /*
-    @$user must be an array (userName : type string,contact :type array)
-    @contact : array(email,phone) / array(email) / array(phone)
+    @$user must be an array (userName : type string,email,phone)
+    
 */
 
-function register($user,$db,$dic)
+function register($user, $db, $dic)
 {
     $output = '';
     $sql = 'SELECT * FROM users';
-    $result = mysqliResultToArray( mysqli_query($db,$sql) );
-    $output = userExist($user,$result) ?array('registration'=>'filed','registrationResult' => 'username , email or phone number already used !'):'newUser';
-    if($output == 'newUser')
-        $output = array('registration'=>'success', 'registrationResult' =>generateAuthKey($user,createUser($user,$db),$dic));
+    $result = mysqliResultToArray(mysqli_query($db, $sql));
+    $output = userExist($user, $result) ? array('registration' => 'filed', 'registrationResult' => 'username , email or phone number already used !') : 'newUser';
+    if ($output == 'newUser')
+        createUser($user, $db);
+        $output = array('registration' => 'success', 'registrationResult' => generateAuthKey($user[0],getUID($db,$user[0]) , $dic));
     return $output;
 }
 
@@ -54,143 +54,143 @@ function validateContact($contact)
  @$user : array with (username,password : md5,contact : empty)
 */
 
-function login($user,$db,$dic)
+function login($user, $db, $dic)
 {
     $output = '';
     $sql = 'SELECT * FROM users';
-    $result = mysqliResultToArray( mysqli_query($db,$sql) );
-    $output = userExist($user,$result) ?'userExist':array('login'=>'filed', 'loginResult' =>'user not exist!');
-    if($output == 'userExist')
-        $output = array('loginResult' => checkPassword($result,$user[1])? generateAuthKey($user,getUID($db,$user[1]),$dic) : 'ERROR:PASSWORD');
+    $result = mysqliResultToArray(mysqli_query($db, $sql));
+    $output = userExist($user, $result) ? 'userExist' : array('login' => 'filed', 'loginResult' => 'user not exist!');
+    if ($output == 'userExist')
+        $output = array('loginResult' => checkPassword($result, $user[1]) ? generateAuthKey($user, getUID($db, $user[1]), $dic) : 'ERROR:PASSWORD');
     return $output;
 }
 
-function checkPassword($row,$pass)
+function checkPassword($row, $pass)
 {
     foreach ($row as $data) {
-        if($data[1] == $pass)
+        if ($data[1] == $pass)
             return true;
     }
     return false;
 }
 
-function createUser($data,mysqli $db)
+function createUser($data, mysqli $db)
 {
-    $userInsert = $db ->prepare('INSERT INTO users(username,password,contact,preferences,friends,inbox,story) VALUES(?,?,?,?,?,?,?,?)');
-    $userInsert ->bind_param('ssssssss',$username,$password,$contact,$pref,$friends,$inbox,$story);
+    
+    $userInsert = $db->prepare('INSERT INTO users(username,password,contact,preferences,friends,inbox,story) VALUES(?,?,?,?,?,?,?);');
+    echo $userInsert->error;
+    $userInsert->bind_param('sssssss', $username, $password, $contact, $pref, $friends, $inbox, $story);
 
     $username = $data[0];
     $password = md5($data[1]);
-    $contact =json_encode( array("email"=>$data[2],"phone"=>$data[3]));
+    $contact = json_encode(array("email" => $data[2], "phone" => $data[3]));
     $pref = json_encode(array(array()));
     $friends = json_encode(array(array()));
     $inbox = json_encode(array(array()));
     $story = json_encode(array(array()));
 
     $userInsert->execute();
-    $userInsert->close();
-    return getUID($db,$data[0]);
     
+    return getUID($db, $data[0]);
 }
 
-function getUID(mysqli $db,$user)
+function getUID(mysqli $db, $user)
 {
-    
+
     return $db->query("SELECT id FROM users WHERE username = '$user'");
 }
 
-function userExist($given,$row)
+function userExist($given, $row)
 {
     foreach ($row as $data) {
-        if($data[0] === $given[0] || strpos($data[2],json_encode($given[2]))) 
+        if ($data[0] === $given[0] || strpos($data[2], json_encode($given[2])))
             return true;
     }
 
     return false;
 }
 
-function mysqliResultToArray($result){
-    $hostArray = array() ;
+function mysqliResultToArray($result)
+{
+    $hostArray = array();
     $k = 0;
     while ($row = mysqli_fetch_array($result)) {
         $hostArray[$k] = $row;
         $k++;
     }
     return $hostArray;
-
 }
 
-function generateAuthKey($user,$id,$dic){
-    
+function generateAuthKey($user, $id, $dic)
+{
 
-    $userPositions =getPositions( str_split($user),$dic);
-    $idPositions =getPositions(str_split($id),$dic);
 
-    $validityTime =getPositions(str_split( floor(microtime(true)*1000) + 24*3600*1000) ,$dic ) ;
+    $userPositions = getPositions(str_split($user), $dic);
+    $idPositions = getPositions(str_split($id), $dic);
 
-    $encryptKey = array(7,3);
-    $decryptKey = array(9,-27);
+    $validityTime = getPositions(str_split(floor(microtime(true) * 1000) + 24 * 3600 * 1000), $dic);
 
-    $treatedUser = encrypt($userPositions,$encryptKey);
-    $treatedid = encrypt($idPositions,$$encryptKey);
-    $treatedTime = encrypt($validityTime,$encryptKey);
+    $encryptKey = array(7, 3);
+    $decryptKey = array(9, -27);
 
-    return base64_encode( toChars($treatedUser,$dic).','. toChars($treatedid,$dic). ','. toChars($treatedTime,$dic) );
+    $treatedUser = encrypt($userPositions, $encryptKey);
+    $treatedid = encrypt($idPositions, $$encryptKey);
+    $treatedTime = encrypt($validityTime, $encryptKey);
+
+    return base64_encode(toChars($treatedUser, $dic) . ',' . toChars($treatedid, $dic) . ',' . toChars($treatedTime, $dic));
 }
 
-function getPositions($array,$dic){
+function getPositions($array, $dic)
+{
     $positions = array();
     $k = 0;
-    for ($i=0; $i <count($array) ; $i++) { 
-        
-        for($j = 0;$j < count($dic);$j++) {
-            if($dic[$j] == $array[$i]) {
+    for ($i = 0; $i < count($array); $i++) {
+
+        for ($j = 0; $j < count($dic); $j++) {
+            if ($dic[$j] == $array[$i]) {
                 $positions[$k] = $j;
-                $k++;     
+                $k++;
             }
         }
     }
 
     return $positions;
-
 }
 
-function encrypt($positions,$key){
+function encrypt($positions, $key)
+{
     $encryptedArray = array();
-    
-    for ($i=0; $i < count($positions); $i++) { 
-        $encryptedArray[$i] = (($positions[$i] * $key[0]) + $key[1]) % 62 > -1 ? (($positions[$i] * $key[0]) + $key[1]) % 62:((($positions[$i] * $key[0]) + $key[1]) % 62)+62;
-    
+
+    for ($i = 0; $i < count($positions); $i++) {
+        $encryptedArray[$i] = (($positions[$i] * $key[0]) + $key[1]) % 62 > -1 ? (($positions[$i] * $key[0]) + $key[1]) % 62 : ((($positions[$i] * $key[0]) + $key[1]) % 62) + 62;
     }
     return $encryptedArray;
 }
 
-function getChars($array,$dic)
+function getChars($array, $dic)
 {
     $char = null;
-    for ($i=0; $i < count($array); $i++) { 
-        for ($j=0; $j < count($dic); $j++) { 
-            if($array[$i] == $j) {
+    for ($i = 0; $i < count($array); $i++) {
+        for ($j = 0; $j < count($dic); $j++) {
+            if ($array[$i] == $j) {
                 $char .= $dic[$j];
             }
-
         }
     }
 
     return $char;
 }
 
-function toChars($array,$dic)
-    {
-        $char = '';
-        for ($i=0; $i < count($array); $i++) { 
-            for ($k=0; $k < count($dic); $k++) { 
-               if( $array[$i] == $k) {
-                    $char .= $dic[$k];
-                }
-    
+function toChars($array, $dic)
+{
+    $char = '';
+    for ($i = 0; $i < count($array); $i++) {
+        for ($k = 0; $k < count($dic); $k++) {
+            if ($array[$i] == $k) {
+                $char .= $dic[$k];
             }
         }
-    
-        return $char;
     }
+
+    return $char;
+}
